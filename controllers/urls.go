cda82cbf8d079a289a9b61879207bb03c0a17127
gothello <marcellosanttos2014@gmail.com>
2021-02-26 16:50:26 -0300

    formated retorn errors

--- controllers/urls.go
+++ controllers/urls.go
@@ -2,12 +2,52 @@ package controllers
 
 import (
 
+	"fmt"
 	"net/http"
+	"encoding/json"
 	"github.com/gothello/go-encurtador/utils"
 )
 
-func Urls(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		utils.ToJson(w, "Hello World", http.StatusOK)
+type reqPost struct {
+	UserID string `json:"user_id"`
+	OriginUrl string `json:"original_url"`
+}
+
+type reqGet struct {
+	Hash string `json:"hash"`
+}
+
+func GetHash(userID string, originalUrl string) (reqGet, error) {
+	retHash := reqGet{
+		Hash: "abcdefghi",
 	}
+
+	fmt.Println(retHash)
+
+	return retHash, nil
+}
+
+func Urls(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		utils.ToError(w, "method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var rp reqPost
+
+	json.NewDecoder(r.Body).Decode(&rp)
+
+	hash, err := GetHash(rp.UserID, rp.OriginUrl)
+	if err != nil {
+		utils.ToError(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	payload, err := json.Marshal(hash)
+	if err != nil {
+		utils.ToError(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	fmt.Println(string(payload))
+
+	utils.ToJson(w, payload, http.StatusCreated)
 }
