be513f8f768977b60f052505c4e5fc560ac6779b
gothello <marcellosanttos2014@gmail.com>
2021-03-16 20:37:00 -0300

    adtional fuction database connect

--- db/db.go
+++ db/db.go
@@ -1,7 +1,37 @@
 package db
 
-import "gopkg.in/mgo.v2"
+import (
 
-func Connect() *mgo.Database {
-	return nil
-}
+	"log"
+	"time"
+	"context"
+	"go.mongo.org/mongo-driver/mongo"
+	"go.mongo.org/mongo-driver/mongo/options"
+)
+
+func Connect() *mongo.Client {
+	conf := config.Load()
+
+	mongoUser := conf.Get("mongouser")
+	mongoPass := conf.Get("mongopass")
+	mongoHost := conf.Get("mongoHost")
+
+	credentials := options.Credentials{
+		Username: mongoUser,
+		password: mongoPass,
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background, 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(context.TODO(), options.Client().SetAuth(credentials).ApplyURI(mongoHost))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return client
+} 
